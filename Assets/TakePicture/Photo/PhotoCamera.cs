499ad51835978ecdd7628afa2ad5578282ae2165
rderbier@gmail.com <rderbier@gmail.com>
2021-04-05 17:18:24 -0700

    add delay and crop image

--- Assets/TakePicture/Photo/PhotoCamera.cs
+++ Assets/TakePicture/Photo/PhotoCamera.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine.Windows.WebCam;
 using TMPro;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using System.Drawing;
 
 
 public class PhotoCamera : MonoBehaviour
@@ -12,6 +15,7 @@ public class PhotoCamera : MonoBehaviour
     PhotoCapture photoCaptureObject = null;
 
     public GameObject PhotoPrefab;
+    public GameObject cursor;
     public TextMeshPro info;
     public bool showPicture = true;
 
@@ -20,23 +24,98 @@ public class PhotoCamera : MonoBehaviour
     float ratio = 1.0f;
     AudioSource shutterSound;
     private MqttHelper mqttHelper;
+    private IMixedRealityGazeProvider gaze;
+    private bool startPicture = false;
+    private Vector3 startPoint;
+    private Vector3 gazePoint;
+    private bool gazeStarted = false;
+    private float timer;
 
     // Use this for initialization
     void Start()
     {
+        gaze = CoreServices.InputSystem.GazeProvider;
+        cursor.SetActive(false);//disable cursor
         shutterSound = GetComponent<AudioSource>() as AudioSource;
         mqttHelper = GetComponent<MqttHelper>() as MqttHelper;
         Debug.Log("File path " + Application.persistentDataPath);
-        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+        // take lower resolution available
+        cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).Last();
 
 
         ratio = (float)cameraResolution.height / (float)cameraResolution.width;
 
         // Create a PhotoCapture object
-        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject) {
+        PhotoCapture.CreateAsync(false, delegate (PhotoCapture captureObject)
+        {
             photoCaptureObject = captureObject;
             Debug.Log("camera ready to take picture");
         });
+        PointerUtils.SetGazePointerBehavior(PointerBehavior.AlwaysOn);
+    }
+    void Update()
+    {
+        float dist;
+        float focus = 0.0f;
+        if (startPicture)
+        {
+            gazePoint = gaze.HitInfo.point;
+            if (gazePoint != null)
+            {
+
+                cursor.transform.position = gazePoint;
+                Vector3 cameraForward = Camera.main.transform.forward;
+                cameraForward.Normalize();
+
+
+                cursor.transform.rotation = Quaternion.LookRotation(cameraForward, Vector3.up);
+                if (gazeStarted == false)
+                {
+                    startPoint = gazePoint;
+                    dist = Vector3.Distance(Camera.main.transform.position, gazePoint);
+                    focus = dist * 0.02f;
+                    cursor.SetActive(true); //enable cursor
+                    gazeStarted = true;
+                    timer = 0.0f;
+                    info.SetText("test gaze started with focus  " + focus);
+                }
+                else
+                {
+                    // if (Vector3.Distance(startPoint, gazePoint) > focus)
+                    // {
+                    //     // gaze moving away 
+                    //     info.SetText("test gaze reset " + Vector3.Distance(startPoint, gazePoint));
+                    //    startPoint = gazePoint;
+                    //     timer = 0.0f;
+                    //     
+                    // }
+                    // else
+                    // {
+                    timer += Time.deltaTime; // add frame duration
+                    if (timer > 1.5f)
+                    {
+                        Debug.Log("test gaze at same point done -> take picture  ");
+                        startPicture = false; // 1 second of gaze at same point 
+                        gazeStarted = false;
+                        cursor.SetActive(false);
+
+                        TakePicture();
+                    }
+                    // }
+                }
+            }
+            else
+            {
+                cursor.SetActive(false);
+                if (gazeStarted == true)
+                {
+                    gazeStarted = false;
+
+                }
+
+            }
+
+        }
 
     }
     public void StopCamera()
@@ -44,6 +123,11 @@ public class PhotoCamera : MonoBehaviour
         // Deactivate our camera
 
         photoCaptureObject?.StopPhotoModeAsync(OnStoppedPhotoMode);
+    }
+    public void StartTakePicture()
+    {
+        gazeStarted = false;
+        startPicture = true;
     }
     public void TakePicture()
     {
@@ -66,7 +150,8 @@ public class PhotoCamera : MonoBehaviour
                 // Take a picture
                 photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
             });
-        } else
+        }
+        else
         {
             info.SetText("camera object is not defined");
         }
@@ -75,18 +160,22 @@ public class PhotoCamera : MonoBehaviour
     void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
     {
         List<byte> imageBufferList = new List<byte>();
+
+        byte[] imageArray;
         if (photoCaptureFrame.pixelFormat == CapturePixelFormat.JPEG)
         {
 
             // Copy the raw IMFMediaBuffer data into our empty byte list.
             photoCaptureFrame.CopyRawImageDataIntoBuffer(imageBufferList);
+            imageArray = imageBufferList.ToArray();
         }
         else
         {
             // Copy the raw image data into our target texture
-            imageBufferList = ConvertAndShowOnDebugPane(photoCaptureFrame);
+            imageArray = ConvertAndShowOnDebugPane(photoCaptureFrame);
 
         }
+
 
 
         string data = System.Convert.ToBase64String(imageBufferList.ToArray());
@@ -96,41 +185,67 @@ public class PhotoCamera : MonoBehaviour
         photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
 
     }
-    private List<byte> ConvertAndShowOnDebugPane(PhotoCaptureFrame photoCaptureFrame)
+    private byte[] ConvertAndShowOnDebugPane(PhotoCaptureFrame photoCaptureFrame)
     {
-        var targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
-        photoCaptureFrame.UploadImageDataToTexture(targetTexture);
-        // Create a gameobject that we can apply our texture to
+        // get image in byte array and crop it
 
-        GameObject newElement = Instantiate<GameObject>(PhotoPrefab);
-        GameObject quad = newElement.transform.Find("Quad").gameObject;
-        Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
-        quadRenderer.material.mainTexture = targetTexture;
-        // new Material(Shader.Find("Unlit/Texture"));
+        //  List<byte> raw = new List<byte>(targetTexture.EncodeToJPG());
 
-        // Set position and rotation 
-        // Bug in Hololens v2 and Unity 2019 about PhotoCaptureFrame not having the location data - March 2020
-        // 
-        // Matrix4x4 cameraToWorldMatrix;
-        // photoCaptureFrame.TryGetCameraToWorldMatrix(out cameraToWorldMatrix);
-        //  Vector3 position = cameraToWorldMatrix.MultiplyPoint(Vector3.zero);
-        //  Quaternion rotation = Quaternion.LookRotation(-cameraToWorldMatrix.GetColumn(2), cameraToWorldMatrix.GetColumn(1));
-        // Vector3 cameraForward = cameraToWorldMatrix * Vector3.forward;
+        try
+        {
+
+            var fullTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+            //targetTexture.LoadRawTextureData(imageBufferList.ToArray()); // use memory stream array to create the texture
+            photoCaptureFrame.UploadImageDataToTexture(fullTexture);
+            int x = cameraResolution.width / 3;
+            int y = cameraResolution.height / 3;
+            int dx = cameraResolution.width / 3;
+            int dy = cameraResolution.height / 3;
+            Color[] pix = fullTexture.GetPixels(x, y, dx, dy);
+            var targetTexture = new Texture2D(dx, dy);
+            targetTexture.SetPixels(pix);
+            targetTexture.Apply();
+            // Create a gameobject that we can apply our texture to
+
+            GameObject newElement = Instantiate<GameObject>(PhotoPrefab);
+            GameObject quad = newElement.transform.Find("Quad").gameObject;
+            Renderer quadRenderer = quad.GetComponent<Renderer>() as Renderer;
+            quadRenderer.material.mainTexture = targetTexture;
+            // new Material(Shader.Find("Unlit/Texture"));
+
+            // Set position and rotation 
+            // Bug in Hololens v2 and Unity 2019 about PhotoCaptureFrame not having the location data - March 2020
+            // 
+            // Matrix4x4 cameraToWorldMatrix;
+            // photoCaptureFrame.TryGetCameraToWorldMatrix(out cameraToWorldMatrix);
+            //  Vector3 position = cameraToWorldMatrix.MultiplyPoint(Vector3.zero);
+            //  Quaternion rotation = Quaternion.LookRotation(-cameraToWorldMatrix.GetColumn(2), cameraToWorldMatrix.GetColumn(1));
+            // Vector3 cameraForward = cameraToWorldMatrix * Vector3.forward;
 
 
 
 
 
 
-        Vector3 cameraForward = Camera.main.transform.forward;
-        cameraForward.Normalize();
-        newElement.transform.position = Camera.main.transform.position + (cameraForward * 0.6f);
+            Vector3 cameraForward = Camera.main.transform.forward;
+            cameraForward.Normalize();
+            newElement.transform.position = Camera.main.transform.position + (cameraForward * 1.0f);
 
-        newElement.transform.rotation = Quaternion.LookRotation(cameraForward, Vector3.up);
-        Vector3 scale = newElement.transform.localScale;
-        scale.y = scale.y * ratio; // scale the entire photo on height
-        newElement.transform.localScale = scale;
-        return new List<byte>(targetTexture.EncodeToJPG());
+            newElement.transform.rotation = Quaternion.LookRotation(cameraForward, Camera.main.transform.up); // align with camera up 
+            Vector3 scale = newElement.transform.localScale;
+            scale.y = scale.y * ratio; // scale the entire photo on height
+            newElement.transform.localScale = scale;
+            List<byte> raw = new List<byte>(targetTexture.EncodeToJPG());
+            return raw.ToArray();
+
+        }
+        catch (System.Exception e)
+        {
+            info.SetText("error " + e.Message);
+            return null;
+        }
+
+        //return raw.ToArray();
     }
 
     void OnStoppedPhotoMode(PhotoCapture.PhotoCaptureResult result)
--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 
 This project shows how to use windows [media capture](https://msdn.microsoft.com/library/windows/apps/windows.media.capture.mediacapture.aspx) in Hololens 2 to access the Camera, take picture send the image to a computer vision (CV) service and display an augmented reality asset close to where the picture was taken depending on the CV result.
 
-For now the project is just a copy of Picture Sample working with latest UNity 2919.4.f1, new XR plugin management and MRTK 2.6.1
+It is inspired by previous work from Joost van Schaik available at
+https://localjoost.github.io/using-azure-custom-vision-object/
+
+and the Picture Sample project I realized for Hololens2.
+
+Using Unity 2919.4.f1, new XR plugin management and MRTK 2.6.1
 
 
 Refer to [Locatable camera info from Microsoft](https://docs.microsoft.com/en-us/windows/mixed-reality/locatable-camera) for details on the Device Camera.
@@ -13,6 +18,19 @@ Refer to [Locatable camera info from Microsoft](https://docs.microsoft.com/en-us
 - MRTK Foundation 2.6.1
 - Unity 2019.4.23f1
 - use of XR plugin management in Unity Project settings
+
+### Design
+Image taken by Hololens Camera is 16:9 and 64.69 deg horizontal fov.
+This is used to compute how to align the picture with the real word.
+At 1m from the view point the image should be 1.26m wide to fit the reality.
+
+As the image is large for image recognition, we decided to crop the thrid of it at the center.
+Croping is done using Texture2D GetPixels and SetPixels.
+
+Image is sent on a MQTT channel as a JPEG base64encoded string.
+
+The ComputerVision result is received on different channel.
+
 
 
 ### Install
